94cd8491cd4c5dc54fba8b3a93ace23f50a0def6
Will <wrussell35@gmail.com>
2014-12-24 00:08:41 -0500

    initial commit, outline complete

---
+++ source/javascripts/all.js
@@ -0,0 +1,12 @@
+//= require jquery/dist/jquery
+//= require foundation/js/foundation.min
+
+//= require app
+//= require script
+// jQuery(document).on("scroll",function(){
+//     if($(document).scrollTop()>60){
+//         $(".contain-to-grid").addClass("undocked");
+//     } else{
+//         $(".contain-to-grid").removeClass("undocked");
+//     }
+// });
